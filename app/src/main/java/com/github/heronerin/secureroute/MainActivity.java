6aa81ee73bb0394ae67074891e0ec4fdddb6dae3
HeronErin <HeronErin@protonmail.com>
2024-03-06 12:55:44 -0500

    A new begining

--- app/src/main/java/com/github/heronerin/secureroute/MainActivity.java
+++ app/src/main/java/com/github/heronerin/secureroute/MainActivity.java
@@ -1,6 +1,5 @@
-package com.github.heronerin.secureroute.login;
+package com.github.heronerin.secureroute;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.github.heronerin.secureroute.Client;
-import com.github.heronerin.secureroute.R;
-
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Client.getOrInit(this);
-        findViewById(R.id.newUser).setOnClickListener((view)->{
-            Intent i = new Intent(this, NewUserActivity.class);
-            startActivity(i);
-        });
-        findViewById(R.id.ExistingUser).setOnClickListener((view)->{
-            Intent i = new Intent(this, ExistingUserActivity.class);
-            startActivity(i);
         });
     }
 }
